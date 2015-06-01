03627ab0d0b6a0c3155f5aa04ccf2333c37e7fb6
schisepo <schisepo@gmail.com>
2015-06-01 21:05:08 +0200

    Finish Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Home | iZone"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Help | iZone"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title","About | iZone"
   end
 
 end
