df43e5d181311114f9fd00333260d4bb350890b7
schisepo <schisepo@gmail.com>
2015-06-02 20:34:13 +0200

    Finished Layout Changes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '4.2.1'
+gem 'bootstrap-sass',       '3.3.4.1'
 gem 'sass-rails',   '5.0.2'
 gem 'uglifier',     '2.5.3'
 gem 'coffee-rails', '4.1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  #get 'static_pages/home'
 
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/contact'
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  get 'signup'=>'users#new'
 
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
