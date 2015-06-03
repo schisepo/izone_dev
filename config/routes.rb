a211fd51bcdcd2cb0243a0a83e9456e6a92b4c84
schisepo <schisepo@gmail.com>
2015-06-03 12:46:10 +0200

    Sign-up working

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'signup'=>'users#new'
 
   root 'static_pages#home'
+
+  resources:users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
