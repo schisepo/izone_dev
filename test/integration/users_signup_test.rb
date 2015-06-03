8e1216b55078e79a15691c53ef1d95eb6b1b92d4
schisepo <schisepo@gmail.com>
2015-06-03 13:45:14 +0200

    misc

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+
   end
 
   test "valid signup information" do
@@ -22,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.FILL_IN
   end
 end
