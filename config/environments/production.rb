359631a96cd6efad87f31f4530472766ad70407c
schisepo <schisepo@gmail.com>
2015-07-30 22:54:40 +0200

    change sendgrid host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fathomless-escarpment-4732.herokuapp.com'
+  # host = 'fathomless-escarpment-4732.herokuapp.com'
+  host = 'izonehub.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
